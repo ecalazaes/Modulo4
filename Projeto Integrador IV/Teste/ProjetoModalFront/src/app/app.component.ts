b9ca5751106df1ee2c85cb9e8047a8f33c30f8dc
Erick Calazães <ecalazaes@gmail.com>
2024-09-20 16:27:09 -0300

    Modulo 4

--- Projeto Integrador IV/Teste/ProjetoModalFront/src/app/app.component.ts
+++ Projeto Integrador IV/Teste/ProjetoModalFront/src/app/app.component.ts
@@ -1,73 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
-import { DataService } from '../services/data.service';
+
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, ModalComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'ProjetoModal';
 
-  filtroNome: string = '';
-  filtroIdade: number | null = null;
-  filtroSelecionado: string = 'nome'; // Inicialize com "nome" por padrão
-  resultado: any[] = []; // Array para armazenar os resultados da requisição
-
-  constructor(private dataService: DataService) { }
-
   ngOnInit(): void {
     initFlowbite();
-  }
-
-  abrirModal() {
-    this.filtroNome = '';
-    this.filtroIdade = null;
-    this.resultado = [];
-  }
-
-  onFilterChange(event: Event): void {
-    const target = event.target as HTMLSelectElement;
-    this.filtroSelecionado = target.value; // Atualiza o filtro selecionado
-    this.filtroNome = '';
-    this.filtroIdade = null;
-    this.resultado = []; // Limpa os resultados quando o filtro é alterado
-  }
-
-  buscar(): void {
-    if (this.filtroSelecionado === 'nome') {
-      this.buscarPorNome();
-    } else if (this.filtroSelecionado === 'idade') {
-      this.buscarPorIdade();
-    }
-  }
-
-  buscarPorNome(): void {
-    if (this.filtroNome) {
-      this.dataService.buscarPorNome(this.filtroNome).subscribe({
-        next: (response: any) => {
-          this.resultado = response; // Armazena os dados recebidos
-        },
-        error: (error) => console.log('Erro na requisição (Nome): ', error),
-      });
-    }
-  }
-
-  buscarPorIdade(): void {
-    if (this.filtroIdade !== null) {
-      this.dataService.buscarPorIdade(this.filtroIdade).subscribe({
-        next: (response: any) => {
-          this.resultado = response; // Armazena os dados recebidos
-        },
-        error: (error) => console.log('Erro na requisição (Idade): ', error),
-      });
-    } else {
-      console.log('Filtro de idade é nulo ou indefinido');
-    }
   }
 }
